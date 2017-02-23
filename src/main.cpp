05615f8a142b11433b8b3ac3efba59fd34bb6832
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2017-02-23 12:52:31 +0700

    lock app startup by datetime

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include "server.h"
-
+#include <iostream>
+#include <QFile>
 #include <QCoreApplication>
 
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
+
+    {
+        QFile file("lock.shts");
+        file.open(QFile::ReadWrite | QFile::Text);
+        QByteArray data = file.readAll();
+
+        QDateTime ts;
+        const QString format = "dd/MM/yyyy HH:mm:ss.zzz";
+        if (!data.isEmpty()) {
+            ts = QDateTime::fromString(QString::fromUtf8(data), format);
+        }
+        else {
+            ts = QDateTime::currentDateTime();
+        }
+
+        if (QDateTime::currentDateTime() < ts) {
+            std::cerr << "Periksa jam dan tanggal pada sistem!" << std::endl;
+            return 2;
+        }
+
+        ts = QDateTime::currentDateTime();
+        file.seek(0);
+        file.write(ts.toString(format).toUtf8());
+        file.close();
+    }
 
     shiftnet::Server server(&app);
 
